f80af9ed2154a60c01a27ab7c2e3453683d9af28
Spark90 <douglas_smcg@hotmail.com>
2020-05-31 20:30:12 -0300

    Criando o Page admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");// o que precisamos para criar nossa pagina
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;// sempre adicionar o user da pagina que vc for usar
 
 $app = new Slim();
 
@@ -12,6 +13,14 @@ $app->config('debug', true);
 $app->get('/', function() {// rota que estou chamando
     
 	$page = new Page();
+	$page->setTpl("index");
+
+});
+
+
+$app->get('/admin', function() {// rota que estou chamando
+    
+	$page = new PageAdmin();
 	$page->setTpl("index");
 
 });
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -10,12 +10,12 @@ class Page{
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(),$tpl_dir = "/views/"){// se nao falar nada no parametro é /views
 
 		$this->options = array_merge($this->defaults, $opts);// merge junta os array e pega o ultimo 
 
 		$config = array(
-		"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+		"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 		"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."views-cache",
 		"debug"         => false// set to false to improve the speed
 	   );
