26468072ebf01fee6bbd3ae284153a1abe55ec87
Spark90 <douglas_smcg@hotmail.com>
2020-06-06 01:11:11 -0300

    detalhes do produto

--- site.php
+++ site.php
@@ -17,21 +17,51 @@ $app->get('/', function() {
 });
 
  $app->get("/categories/:idcategory", function($idcategory){
-	 	
 
-	 	 $category = new Category();
-	 	 $category->get((int)$idcategory);//pega apenas se for um numero da categoria
-	 	 $page = New Page();
-	 	 $page->setTpl("category",[
-	 	 	'category'=>$category->getValues(),
-	 	 	'products'=>Product::checkList($category->getProducts())
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-	 	 ]);
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['pages']; $i++) { 
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
+
+	$page = new Page();
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>$pagination["data"],
+		'pages'=>$pages
+	]);
+
+});
+
+$app->get("/products/:desurl", function($desurl){
+	$product = new Product();
+	$product->getFromURL($desurl);
+	$page = new Page();
+	
+	$page->setTpl("product-detail",[
+		"product"=>$product->getValues(),
+		"categories"=>$product->getCategories()
 
 
 
-	 	 });
+	]);
 
 
+
+
+
+});
 
  ?>
