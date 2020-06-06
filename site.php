ac1880a618af38856280f15b4e4ad2c37ec2803f
Spark90 <douglas_smcg@hotmail.com>
2020-06-06 17:29:42 -0300

    Carrinho calculo de frete

--- site.php
+++ site.php
@@ -68,7 +68,8 @@ $app->get("/cart", function(){
 	$page = new Page();
 	$page->setTpl("cart",[
 		"cart"=>$cart->getValues(),
-		"products"=>$cart->getProducts()
+		"products"=>$cart->getProducts(),
+		"error"=>Cart::getMsgError()
 
 
 
@@ -126,5 +127,19 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 
 
 });
+
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+
 
  ?>
